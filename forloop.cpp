9a210b5c2a08f69389b2b9a5d92bebf3adf8e97f
ADARSH RAJ <adarshraaz.23@gmail.com>
2026-02-04 13:18:54 +0530

    Add files via upload

---
+++ forloop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number n"<<endl;
+    cin>>n;
+    for(int i=1;i<=5;i++){
+        cout<<" My dream is to become a software engineer"<<endl;
+    }
+    return 0;
+}
